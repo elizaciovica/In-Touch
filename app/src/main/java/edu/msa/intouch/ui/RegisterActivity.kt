7c724c1e357e2cf2b1a33331eb8ab5b4152e1981
dianamiscuta <dianamiscuta@yahoo.ro>
2022-10-18 15:34:39 +0300

    Worked on register page frontend

---
+++ app/src/main/java/edu/msa/intouch/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package edu.msa.intouch.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.msa.intouch.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
