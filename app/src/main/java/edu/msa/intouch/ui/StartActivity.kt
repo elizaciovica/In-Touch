d6fb1864111b5122267d9091f15aa86979f2308e
dianamiscuta <dianamiscuta@yahoo.ro>
2022-10-19 22:14:07 +0300

    Implemented navigation from start page to register page

--- app/src/main/java/edu/msa/intouch/ui/StartActivity.kt
+++ app/src/main/java/edu/msa/intouch/ui/StartActivity.kt
@@ -1,7 +1,9 @@
 package edu.msa.intouch.ui
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import edu.msa.intouch.R
 
@@ -9,5 +11,10 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        val registerButton: Button = findViewById(R.id.signUpButton)
+        registerButton.setOnClickListener {
+            val register = Intent(this@StartActivity, RegisterActivity::class.java)
+            startActivity(register)
+        }
     }
 }
