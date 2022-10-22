183b3af903ef45c15e6441e279e47da479e7ff25
Eliza Ciovica <elizaciovica0208@gmail.com>
2022-10-22 16:01:16 +0300

    Added basic home page with log out option on profile icon click

---
+++ app/src/main/java/edu/msa/intouch/ui/HomeActivity.kt
@@ -0,0 +1,49 @@
+package edu.msa.intouch.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Button
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
+import edu.msa.intouch.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setBinding()
+
+        val button : Button = binding.homeIcon
+
+        val showPopUp = PopupMenu(
+            this,
+            button
+        )
+
+        showPopUp.menu.add(Menu.NONE, 0, 0, "Upload profile photo")
+        showPopUp.menu.add(Menu.NONE, 1, 1, "Log Out")
+
+        showPopUp.setOnMenuItemClickListener { menuItem ->
+            val id = menuItem.itemId
+            if (id ==1 ){
+                startActivity(Intent(this, RegisterActivity::class.java))
+            }
+            false
+        }
+
+        button.setOnClickListener {
+            showPopUp.show()
+        }
+
+    }
+
+    private fun setBinding() {
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
