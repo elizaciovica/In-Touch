6d60d6b577166698b53766cf38ccbbc233d1dde5
Eliza Ciovica <elizaciovica0208@gmail.com>
2022-11-04 20:56:09 +0200

    Changed from h2 database to PostgreSQL database

--- api/src/main/java/edu/msa/api/InTouchApiApplication.java
+++ api/src/main/java/edu/msa/api/InTouchApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InTouchApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InTouchApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InTouchApiApplication.class, args);
+    }
 
 }
