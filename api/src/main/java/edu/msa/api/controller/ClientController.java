24cefbfc2265676c6045695ed857d3bfe34a42ee
Eliza Ciovica <elizaciovica0208@gmail.com>
2022-11-13 17:21:24 +0200

    Remove unused imports in ClientController

--- api/src/main/java/edu/msa/api/controller/ClientController.java
+++ api/src/main/java/edu/msa/api/controller/ClientController.java
@@ -1,23 +1,13 @@
 package edu.msa.api.controller;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseToken;
 import edu.msa.api.model.Client;
 import edu.msa.api.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.security.Principal;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
