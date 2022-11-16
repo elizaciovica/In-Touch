6f5b202e698dd4ff48efe7abe4b1014e77e441dd
dianamiscuta <dianamiscuta@yahoo.ro>
2022-11-16 13:36:57 +0200

    Added missing features to connections pages.

--- InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/ConnectionListActivity.kt
+++ InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/ConnectionListActivity.kt
@@ -1,14 +1,42 @@
 package edu.msa.intouch.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Button
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
 import edu.msa.intouch.R
+import edu.msa.intouch.databinding.ActivityConnectionListBinding
 
 class ConnectionListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConnectionListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connection_list)
+        val button: Button = binding.homeIcon
+
+        val showPopUp = PopupMenu(
+            this,
+            button
+        )
+
+        showPopUp.menu.add(Menu.NONE, 0, 0, "Upload profile photo")
+        showPopUp.menu.add(Menu.NONE, 1, 1, "Log Out")
+
+        showPopUp.setOnMenuItemClickListener { menuItem ->
+            val id = menuItem.itemId
+            if (id == 1) {
+                startActivity(Intent(this, RegisterActivity::class.java))
+                finish()
+            }
+            false
+        }
+
+        button.setOnClickListener {
+            showPopUp.show()
+        }
     }
 }
