f1688e1f390188ec64c4944028b0019cedd2181a
dianamiscuta <dianamiscuta@yahoo.ro>
2022-12-07 00:24:55 +0200

    Sent data from SharedConnection to Chat in order to display the correct user.

--- InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/ChatActivity.kt
+++ InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/ChatActivity.kt
@@ -2,6 +2,7 @@ package edu.msa.intouch.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import edu.msa.intouch.R
 import edu.msa.intouch.databinding.ActivityChatBinding
 
@@ -12,10 +13,18 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
         setBiding()
+        setUserDetails()
     }
 
     private fun setBiding() {
         binding = ActivityChatBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun setUserDetails(){
+        val usernameTextView : TextView = findViewById(R.id.username)
+        val clientList = ConnectionListActivity().clientList
+        val selectedUser = clientList.find{it.firebaseId == (intent.extras?.getString("userId"))}
+        usernameTextView.text = selectedUser?.firstName + " " + selectedUser?.lastName
     }
 }
--- InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/SharedConnectionActivity.kt
+++ InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/SharedConnectionActivity.kt
@@ -1,11 +1,12 @@
 package edu.msa.intouch.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import edu.msa.intouch.R
 import edu.msa.intouch.databinding.ActivitySharedConnectionBinding
+
 
 class SharedConnectionActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySharedConnectionBinding
@@ -13,8 +14,8 @@ class SharedConnectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setBinding()
-        setUserDetails()
         initializeButtons()
+        setUserDetails()
     }
 
     private fun setBinding(){
@@ -24,9 +25,9 @@ class SharedConnectionActivity : AppCompatActivity() {
 
     private fun initializeButtons() {
         binding.chatBtn.setOnClickListener {
-            val intent = Intent(this, ChatActivity::class.java)
-            startActivity(intent)
-            finish()
+            val chatIntent = Intent(this, ChatActivity::class.java)
+            chatIntent.putExtra("userId", intent.extras?.getString("userId"))
+            startActivity(chatIntent)
         }
 
     }
