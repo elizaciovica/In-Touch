d05c0473f9bdb1a63f9448b0906dace0c5b30f7e
dianamiscuta <dianamiscuta@yahoo.ro>
2023-01-20 00:31:32 +0200

    Implemented add event, show events, and delete event

--- InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/CalendarActivity.kt
+++ InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/CalendarActivity.kt
@@ -1,29 +1,58 @@
 package edu.msa.intouch.ui
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import edu.msa.intouch.R
 import edu.msa.intouch.databinding.ActivityCalendarBinding
-import edu.msa.intouch.databinding.ActivitySharedConnectionBinding
+import edu.msa.intouch.model.Client
+import edu.msa.intouch.model.Event
+import edu.msa.intouch.model.Note
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CalendarActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCalendarBinding
     private var storageRef = Firebase.storage
+    private var firebaseFirestore = Firebase.firestore
+    private lateinit var selectedDate : String
+    private lateinit var dateText : TextView
+    private lateinit var eventAdapter: FirestoreRecyclerAdapter<Event, EventViewHolder>
+    private lateinit var staggeredGridLayoutManager : StaggeredGridLayoutManager
+    private lateinit var documentReference : DocumentReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setBinding()
         setNavigation(this)
         getProfilePicture()
+        initializeButtons()
+        setUserDetails()
+        selectDateAction()
+        getEvents()
     }
 
     private fun setBinding() {
@@ -65,21 +94,137 @@ class CalendarActivity : AppCompatActivity() {
         }
     }
 
+    private fun setUserDetails(){
+        val usernameTextView : TextView = findViewById(R.id.username)
+        val selectedUser =
+            Json.decodeFromString<Client>(intent.getSerializableExtra("selectedUser") as String)
+        usernameTextView.text = selectedUser!!.firstName + " " + selectedUser!!.lastName
+    }
+
+    @SuppressLint("SimpleDateFormat")
     private fun initializeButtons() {
+        dateText = findViewById(R.id.dateText)
+        val date = System.currentTimeMillis()
+        val sdf = SimpleDateFormat("dd MMM yyyy")
+        val dateString = sdf.format(date)
+        dateText.text = dateString
+
         binding.addEventBtn.setOnClickListener {
-            // open popup with add event
-//            val chatIntent = Intent(this, ChatActivity::class.java)
-//            val selectedUser = intent.getSerializableExtra("selectedUser") as String
-//            chatIntent.putExtra("selectedUser", selectedUser)
-//            startActivity(chatIntent)
+            val createEventIntent = Intent(this, CreateEventActivity::class.java)
+            val selectedUser = intent.getSerializableExtra("selectedUser") as String
+            createEventIntent.putExtra("selectedUser", selectedUser)
+            createEventIntent.putExtra("selectedDate", dateText.text)
+            startActivity(createEventIntent)
         }
+    }
+
+    private fun selectDateAction(){
+
+        val myCalendar = Calendar.getInstance()
+        
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth -> 
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel(myCalendar)
+        }
+
 
         binding.selectDateBtn.setOnClickListener {
             // open popup with datepicker
-//            val chatIntent = Intent(this, ChatActivity::class.java)
-//            val selectedUser = intent.getSerializableExtra("selectedUser") as String
-//            chatIntent.putExtra("selectedUser", selectedUser)
-//            startActivity(chatIntent)
+            DatePickerDialog(this, datePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+    }
+
+    private fun updateLabel(myCalendar: Calendar) {
+        val sdf = SimpleDateFormat("dd MMM yyyy")
+        val dateFormatted = sdf.format(myCalendar.time)
+        dateText.text = dateFormatted
+        getEvents()
+    }
+
+    private fun getEvents() {
+        val selectedUser =
+            Json.decodeFromString<Client>(intent.getSerializableExtra("selectedUser") as String)
+        val currentUser = FirebaseAuth.getInstance().currentUser!!.uid
+
+        val bitEncode = (selectedUser.firebaseId + currentUser).encodeToByteArray()
+        val sumArray = bitEncode.sum()
+
+        val query = firebaseFirestore.collection("events").document(sumArray.toString()).collection(dateText.text.toString())
+        val allUsersEvents = FirestoreRecyclerOptions.Builder<Event>().setQuery(query, Event::class.java).build()
+
+        eventAdapter = EventFirestoreRecyclerAdapter(allUsersEvents)
+        staggeredGridLayoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+        binding.eventsRecyclerView.layoutManager = staggeredGridLayoutManager
+        binding.eventsRecyclerView.adapter = eventAdapter
+        binding.eventsRecyclerView.adapter?.notifyDataSetChanged()
+        onStart()
+    }
+
+    private inner class EventViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        fun setEventDetails(title: String, time: String) {
+            val titleView = view.findViewById<TextView>(R.id.eventTitle)
+            val timeView = view.findViewById<TextView>(R.id.eventTime)
+            titleView.text = title
+            timeView.text = time
+        }
+    }
+
+    private inner class EventFirestoreRecyclerAdapter(options: FirestoreRecyclerOptions<Event>)
+        : FirestoreRecyclerAdapter<Event, CalendarActivity.EventViewHolder>(options) {
+
+        override fun onBindViewHolder(eventViewHolder: CalendarActivity.EventViewHolder, position: Int, event: Event) {
+            eventViewHolder.setEventDetails(event.title, event.time)
+
+            val docId = eventAdapter.snapshots.getSnapshot(position).id
+
+            onDelete(eventViewHolder.itemView, docId)
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_event, parent, false)
+            return EventViewHolder(view)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        eventAdapter!!.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (eventAdapter != null) {
+            eventAdapter!!.stopListening()
+        }
+    }
+
+    fun onDelete(view: View, docId: String) {
+        val deleteButton = view.findViewById<ImageButton>(R.id.deleteEventBtn)
+        deleteButton.setOnClickListener() {
+            val selectedUser =
+                Json.decodeFromString<Client>(intent.getSerializableExtra("selectedUser") as String)
+            val currentUser = FirebaseAuth.getInstance().currentUser!!.uid
+
+            val bitEncode = (selectedUser.firebaseId + currentUser).encodeToByteArray()
+            val sumArray = bitEncode.sum()
+
+            documentReference = firebaseFirestore.collection("events").document(sumArray.toString())
+                .collection(dateText.text.toString()).document(docId)
+            documentReference.delete().addOnSuccessListener {
+                Toast.makeText(applicationContext, "Event deleted", Toast.LENGTH_SHORT)
+                    .show()
+            }
+                .addOnFailureListener() {
+                    Toast.makeText(applicationContext, "Failed to delete", Toast.LENGTH_SHORT)
+                        .show()
+                }
         }
     }
 }
--- InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/CreateEventActivity.kt
+++ InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/CreateEventActivity.kt
@@ -1,12 +1,169 @@
 package edu.msa.intouch.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import edu.msa.intouch.R
+import edu.msa.intouch.databinding.ActivityCreateEventBinding
+import edu.msa.intouch.model.Client
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.util.*
 
 class CreateEventActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateEventBinding
+    private var storageRef = Firebase.storage
+    private var firebaseFirestore = Firebase.firestore
+    private lateinit var timePicker : TimePicker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_event)
+        setBinding()
+        getMenu()
+        setNavigation(this)
+        getProfilePicture()
+        setDate()
+        initButtons()
+        createEvent()
+    }
+
+    private fun setBinding() {
+        binding = ActivityCreateEventBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun initButtons() {
+        timePicker = findViewById(R.id.timePicker1)
+        timePicker.setIs24HourView(true)
+        timePicker.setHour(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+    }
+
+    private fun getMenu() {
+        val button: ImageButton = binding.homeIcon
+
+        val showPopUp = PopupMenu(
+            this,
+            button
+        )
+
+        showPopUp.menu.add(Menu.NONE, 0, 0, "See profile details")
+        showPopUp.menu.add(Menu.NONE, 1, 1, "Log Out")
+
+        showPopUp.setOnMenuItemClickListener { menuItem ->
+            val id = menuItem.itemId
+            if (id == 1) {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+            if (id == 0) {
+                startActivity(Intent(this, ProfileActivity::class.java))
+                finish()
+            }
+            false
+        }
+
+        button.setOnClickListener {
+            showPopUp.show()
+        }
+    }
+
+    private fun setNavigation(activity: Activity) {
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener { item ->
+            if (item.itemId == R.id.page_1) {
+                activity.startActivity(Intent(activity, HomeActivity::class.java))
+                true
+            }
+            if (item.itemId == R.id.page_2) {
+                activity.startActivity(Intent(activity, ConnectionActivity::class.java))
+                true
+            }
+            false
+        }
+    }
+
+    private fun getProfilePicture() {
+        val userId = FirebaseAuth.getInstance().currentUser!!.uid
+        var islandRef = storageRef.reference.child("images/$userId")
+        val ONE_MEGABYTE: Long = 1024 * 1024 * 10
+        islandRef.getBytes(ONE_MEGABYTE).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeByteArray(it, 0, it.size)
+            binding.homeIcon.background = BitmapDrawable(
+                resources,
+                Bitmap.createScaledBitmap(
+                    bitmap,
+                    binding.homeIcon.width,
+                    binding.homeIcon.height,
+                    false
+                )
+            )
+        }.addOnFailureListener {
+            // Handle any errors
+        }
+    }
+
+    private fun setDate() {
+        val selectedDate = intent.getSerializableExtra("selectedDate") as String
+        val selectedDateText : TextView = findViewById(R.id.selectedDateText)
+        selectedDateText.text = selectedDate
+    }
+
+    private fun createEvent(){
+        val currentUser = FirebaseAuth.getInstance().currentUser!!.uid
+        val selectedUser =
+            Json.decodeFromString<Client>(intent.getSerializableExtra("selectedUser") as String)
+
+        binding.saveBtn.setOnClickListener {
+            var title = binding.createTitleEvent.text.toString()
+            val timeHour = timePicker.hour.toString()
+            val timeMinute : String
+            if(timePicker.minute > 9) {
+                timeMinute = timePicker.minute.toString()
+            }
+            else {
+                timeMinute = "0" + timePicker.minute.toString()
+            }
+            var time = "$timeHour:$timeMinute"
+
+            val bitEncode = (selectedUser.firebaseId + currentUser).encodeToByteArray()
+            val sumArray = bitEncode.sum()
+
+            if (title.isEmpty()) {
+                Toast.makeText(applicationContext, "Title is required", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                val selectedDate = intent.getSerializableExtra("selectedDate") as String
+                val documentReference = firebaseFirestore.collection("events").document(sumArray.toString()).collection(selectedDate).document()
+
+                val event = HashMap<String, String>()
+                event["title"] = title
+                event["time"] = time
+
+                documentReference.set(event).addOnSuccessListener {
+                    Toast.makeText(applicationContext, "Event created successfully", Toast.LENGTH_LONG)
+                        .show()
+
+                    val eventIntent = Intent(this, CalendarActivity::class.java)
+                    val selectedUser = intent.getSerializableExtra("selectedUser") as String
+                    eventIntent.putExtra("selectedUser", selectedUser)
+                    eventIntent.putExtra("selectedDate", selectedDate)
+                    startActivity(eventIntent)
+
+                } .addOnFailureListener {
+                    Toast.makeText(applicationContext, "Failed to create note", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
     }
 }
