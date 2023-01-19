eb97fe45623c835e80f1f8cdd301b604d020c5d5
dianamiscuta <dianamiscuta@yahoo.ro>
2023-01-19 19:24:22 +0200

    Created create event activity

---
+++ InTouchKotlin/app/src/main/java/edu/msa/intouch/ui/CreateEventActivity.kt
@@ -0,0 +1,12 @@
+package edu.msa.intouch.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.msa.intouch.R
+
+class CreateEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_event)
+    }
+}
